f206043c40e930f2ae63331e52beb411377ec99c
Adity Shaw <86835651+adityraj01@users.noreply.github.com>
2023-07-15 09:25:37 +0530

    Add files via upload

---
+++ backend/springboot-backed/src/main/java/com/example/demo/SpringbootBackedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackedApplication.class, args);
+	}
+
+}
