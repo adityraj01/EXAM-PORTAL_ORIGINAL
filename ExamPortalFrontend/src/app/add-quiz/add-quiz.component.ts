645e0d58083ef9475c63fe3b64b8e3143cebf05c
Adity Shaw <adityshaw99@gmail.com>
2023-08-15 13:55:31 +0530

    add quiz page

--- ExamPortalFrontend/src/app/add-quiz/add-quiz.component.ts
+++ ExamPortalFrontend/src/app/add-quiz/add-quiz.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../service/category.service';
+import Swal from 'sweetalert2';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { QuizService } from '../service/quiz.service';
+
+interface Category {
+  cid: number;
+  title: string;
+  // other properties if available
+}
 
 @Component({
   selector: 'app-add-quiz',
@@ -6,17 +16,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-quiz.component.css']
 })
 export class AddQuizComponent implements OnInit{
-  categories=[
-    {
-      cid:23,
-      title:'Programming'
+  categories: Category[] = [];
+
+  quizData={
+    title:'',
+    description:'',
+    maxMarks:'',
+    numberOfQuestions:'',
+    active:true,
+    category:{
+      cid:'',
     },
-    {
-      cid:23,
-      title:'Programming'
+  }
+  constructor( private _cat: CategoryService, private _snack:MatSnackBar, private _quiz:QuizService){}
+  ngOnInit(): void {
+    this._cat.categories().subscribe(
+      (data:any)=>{
+        //categories load
+        this.categories = data;
+        console.log(this.categories);
+      },
+      (error)=>{
+        console.log(error);
+        Swal.fire('Error !!','Server Error !!','error');
+      }
+    );
+  }
+  addQuiz(){
+    if(this.quizData.title.trim() == '' || this.quizData.title == null){
+      this._snack.open('Title Required !!','',{
+        duration:3000,
+      });
+      return; 
     }
-  ]
-  constructor(){}
-  ngOnInit(): void {}
+    this._quiz.addQuiz(this.quizData).subscribe((data:any)=>{
+        Swal.fire('Success','quiz is added', 'success')
+        this.quizData={
+          title:'',
+          description:'',
+          maxMarks:'',
+          numberOfQuestions:'',
+          active:true,
+          category:{
+            cid:'',
+          },
+        };
+      },
+      (error:any)=>{
+        Swal.fire('error','Error while adding quiz','error');
+        console.log(error)
+
+      }
+    )
+  }
+
+  
 
 }
--- ExamPortalFrontend/src/app/service/quiz.service.ts
+++ ExamPortalFrontend/src/app/service/quiz.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,12 @@ import { Injectable } from '@angular/core';
 export class QuizService {
   baseUrl = `http://localhost:8080`;
   constructor(private _http:HttpClient){}
+
   public quizzes(){
     return this._http.get(`${this.baseUrl}/quiz/`);
+  }
+
+  public addQuiz(quiz: any): Observable<any>{
+    return this._http.post(`${this.baseUrl}/quiz/`, quiz);
   }
 }
